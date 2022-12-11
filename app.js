7a0026fca1691fbf6b6df332557c3df46fb7ec8c
Karen Nason <knason4@gmail.com>
2022-12-11 15:08:03 -0600

    add files

---
+++ app.js
@@ -0,0 +1,94 @@
+// In this exercise, you’ll refactor some ES5 code into ES2015. 
+// Write your code in the sections with a 
+// comment to “Write an ES2015 Version”.
+
+// **** SAME KEYS AND VALUES *****
+
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+// **** Write an ES2015 Version ****
+
+const createInstructor = (firstName, lastName) => {
+    return {
+        firstName,
+        lastName
+    }
+    
+};
+
+
+/* Computed Propery Names */
+
+/*
+var favoriteNumber = 42;
+var instructor = {
+    firstName: 'Colt'
+}
+
+instructor[favoriteNumber] = 'That is my favorite!';
+
+*/
+
+/* Write an ES2015 Version */
+
+let favoriteNumber = 42;
+
+const instructor = {
+    firstName: 'Colt',
+    [favoriteNumber]: 'That is my favorite!'
+}
+
+/* Object Methods */
+
+/*
+var instructor = {
+    firstName: 'Colt',
+    sayHi: function() {
+        return 'Hi!
+    },
+    sayBye: function() {
+        return this.firstName + "says bye!"
+    }
+}
+*/
+
+/* Write an ES2015 Version */
+
+//instructor.sayBye() returns 'Colt says bye!'
+const instructor2 = {
+    firstName: 'Colt',
+    sayHi() {
+        return 'Hi!'
+    },
+    sayBye() {
+        return this.firstName + " says bye!";
+    }
+}
+
+/* createAnimal Function 
+Write a function which generates an animal object. The function
+should accept 3 arguments:
+    species: the species of animal('cat', 'dog')
+    verb: a string used to name a function ('bark', 'bleet')
+    noise: a string to be printed when above function is called ('woof', 'bleet')
+
+    const d = createAnimal("dog", "bark", "Woooof!");
+    {species: 'dog', bark: ƒ}
+    d.bark()
+    'Woooof!'
+*/
+
+function createAnimal(species, verb, noise) {
+    return {
+        species,
+        [verb] () {
+            return noise;
+        }
+    }
+}
+
